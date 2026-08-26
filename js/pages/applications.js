f54bd13a6607add4c860f97139dd2477468eb27f
Vikas <vikas74988@gmail.com>
2026-08-26 13:03:05 +0530

    feat: create applications page UI

---
+++ js/pages/applications.js
@@ -0,0 +1,11 @@
+// =========================================
+// CareerTrack - Applications Page
+// =========================================
+
+console.log("Applications page initialized.");
+
+const addApplicationBtn = document.getElementById("addApplicationBtn");
+
+addApplicationBtn.addEventListener("click", () => {
+    console.log("Add Application clicked");
+});
